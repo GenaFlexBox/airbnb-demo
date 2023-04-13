f3d7da1687ce861e39546a6062b392173de53187
gn.kopylov <gn.kopylov@qsoft.ru>
2023-04-13 14:24:10 +0500

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
 	const listings = await getListings();
 	const currentUser = await getCurrentUser();
 
-	if (listings.length === 0) {
+	if (listings?.length === 0) {
 		return (
 			<ClientOnly>
 				<EmptyState showReset />
@@ -22,7 +22,7 @@ export default async function Home() {
         <ClientOnly>
             <Container>
                 <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-					{listings.map((listing: SafeListing) => {
+					{listings?.map((listing: SafeListing) => {
 						return (
 							<ListingCard key={listing.id} data={listing} currentUser={currentUser}/>
 						)
